d6f5fa7e4b41b10776c6f70c2bcd5b8a88b387b1
M Murali Karthick <165348508+Murali47k@users.noreply.github.com>
2024-04-29 19:14:12 +0530

    Update-1_VGG-16_model.py

--- ALL CNN MODEL/VGG-16_model.py
+++ ALL CNN MODEL/VGG-16_model.py
@@ -20,20 +20,20 @@ EPOCHS = 20
 data_dir = 'drive/MyDrive/Colab Notebooks/newXray' # dataset location
 
 # Define data generator with validation split
-datagen = ImageDataGenerator(rescale=1./255,
+gendata = ImageDataGenerator(rescale=1./255,
                              shear_range=0.2,
                              zoom_range=0.2,
                              horizontal_flip=True,
                              validation_split=0.20)
 
-train_generator = datagen.flow_from_directory(data_dir,
+train_data = gendata.flow_from_directory(data_dir,
                                               target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
                                               batch_size=BATCH_SIZE,
                                               class_mode='categorical',
                                               subset='training')  # Training data
 
 # Generate validation dataset
-validation_generator = datagen.flow_from_directory(data_dir,
+validation_data = gendata.flow_from_directory(data_dir,
                                                    target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
                                                    batch_size=BATCH_SIZE,
                                                    class_mode='categorical',
@@ -47,7 +47,7 @@ model = Sequential([
     Flatten(),
     Dense(256, activation='relu'),
     Dropout(0.5),
-    Dense(train_generator.num_classes, activation='softmax')
+    Dense(train_data.num_classes, activation='softmax')
 ])
 
 # Freeze the base_model layers
@@ -61,13 +61,13 @@ model.compile(optimizer='adam',
 
 # Train the model
 history = model.fit(train_generator,
-                    steps_per_epoch=train_generator.samples // BATCH_SIZE,
+                    steps_per_epoch=train_data.samples // BATCH_SIZE,
                     epochs=EPOCHS,
-                    validation_data=validation_generator,
-                    validation_steps=validation_generator.samples // BATCH_SIZE)
+                    validation_data=validation_data,
+                    validation_steps=validation_data.samples // BATCH_SIZE)
 
 # Evaluate the model
-loss, accuracy = model.evaluate(validation_generator, verbose=1)
+loss, accuracy = model.evaluate(validation_data, verbose=1)
 print("Validation Accuracy: {:.2f}%".format(accuracy * 100))
 
 # Load and preprocess the image
@@ -81,7 +81,7 @@ img_array = preprocess_input(img_array)
 predictions = model.predict(img_array)
 
 # Interpret the predictions
-class_indices = train_generator.class_indices
+class_indices = train_data.class_indices
 predicted_class = list(class_indices.keys())[np.argmax(predictions)]
 
 print("Predicted class:", predicted_class)
