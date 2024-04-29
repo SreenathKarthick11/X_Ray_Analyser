3117e1b5e6e11b33871f102494850f19e5b14df2
M Murali Karthick <165348508+Murali47k@users.noreply.github.com>
2024-04-29 19:04:26 +0530

    Update-1_Confusion_matrix.py

--- ALL CNN MODEL/Confusion_matrix.py
+++ ALL CNN MODEL/Confusion_matrix.py
@@ -4,14 +4,14 @@ import tensorflow as tf
 import numpy as np
 
 # Make predictions on validation data
-predictions = np.argmax(model.predict(validation_generator), axis=-1)
-labels = validation_generator.classes
+predictions = np.argmax(model.predict(validation_data), axis=-1)
+labels = validation_data.classes
 
 # Compute confusion matrix
 conf = tf.math.confusion_matrix(labels=labels, predictions=predictions)
 
 # Plot confusion matrix as a heatmap
-sns.heatmap(conf, annot=True, cmap='Blues', yticklabels=validation_generator.class_indices.keys(), xticklabels=validation_generator.class_indices.keys())
+sns.heatmap(conf, annot=True, cmap='Blues', yticklabels=validation_data.class_indices.keys(), xticklabels=validation_data.class_indices.keys())
 plt.xlabel('Predicted labels')
 plt.ylabel('True labels')
 plt.title('Confusion Matrix')
